9f71235923f482eea0e9a8e3522c4a7935269aa7
Elpredicador123 <agentepredicador@gmail.com>
2020-10-03 12:39:37 -0500

    challenge(session-01): jesus saavedra

---
+++ session-01/tasks/Jesus Saavedra/my-first-app/src/main/java/io/jesussaavedra/myfirstapp/Education.java
@@ -0,0 +1,16 @@
+package io.jesussaavedra.myfirstapp;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Education implements Serializable{
+    private String university;
+    private String career;
+}
